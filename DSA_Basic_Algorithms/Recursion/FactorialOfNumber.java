213764dfd3d25770dc626bf0f4de96b53086e682
munibalaji-dev <yadavbalaji419@gmail.com>
2026-03-09 22:53:50 +0530

    Commited new recursion file

---
+++ DSA_Basic_Algorithms/Recursion/FactorialOfNumber.java
@@ -0,0 +1,14 @@
+package DSA_Basic_Algorithms.Recursion;
+
+public class FactorialOfNumber {
+    static int fact(int n){
+        if (n == 0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        int n =5;
+        System.out.println(fact(n));
+    }
+}
