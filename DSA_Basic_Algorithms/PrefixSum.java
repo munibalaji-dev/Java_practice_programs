8439bd21d825ea963db9eb766e8390f2e70ed873
munibalaji-dev <yadavbalaji419@gmail.com>
2026-03-09 11:57:34 +0530

    Adding Prefix Sum Program

---
+++ DSA_Basic_Algorithms/PrefixSum.java
@@ -0,0 +1,28 @@
+package DSA_Basic_Algorithms;
+
+public class PrefixSum {
+    public static void main(String[] args) {
+
+        int[] arr = {2,4,6,8,10};
+
+//        int n = arr.length;
+//        int[] prefix = new int[n];
+
+        int[] prefix = new int[arr.length];
+
+        prefix[0] = arr[0];
+
+        for(int i=1;i< arr.length;i++){
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+        int L = 1;
+        int R = 3;
+        int sum;
+        if(L == 0)
+            sum = prefix[R];
+        else
+            sum = prefix[R] - prefix[L-1];
+
+        System.out.println("Range of sum = "+sum);
+    }
+}
