343fd4f07e045dafce804e944d55912d84e8f3b6
munibalaji-dev <yadavbalaji419@gmail.com>
2026-03-16 23:04:09 +0530

    Commit changes in maxsubarray

--- DSA_Basic_Algorithms/FindMaximumSubArraySum.java
+++ DSA_Basic_Algorithms/FindMaximumSubArraySum.java
@@ -3,7 +3,7 @@ package DSA_Basic_Algorithms;
 // Using Kaden's Algorithm
 public class FindMaximumSubArraySum {
 
-    static int subArraySum(int[] arr, int n){
+    static int maxSubarray(int[] arr){
         int maxSum = Integer.MIN_VALUE;
         int currentSum =0;
         for (int i=0;i<arr.length;i++){
@@ -25,6 +25,6 @@ public class FindMaximumSubArraySum {
         int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
         int n = 7;
 
-        System.out.println(subArraySum(arr, n));
+        System.out.println(maxSubarray(arr));
     }
 }
